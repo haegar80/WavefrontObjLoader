1bd3fd7772e6441d17eebd8260549079215db854
maesi <marcelfuchs@gmx.ch>
2018-03-31 15:48:25 +0200

    Upgraded to Qt 5.10.1 and VS2017

--- WavefrontObjLoader/main.cpp
+++ WavefrontObjLoader/main.cpp
@@ -9,8 +9,8 @@ int main(int argc, char *argv[])
     const int cSize = 1000;
 
     ObjLoader objLoader;
-    std::string wavefrontDir = "Wavefront/building/";
-    std::string fileName = "building.obj";
+    std::string wavefrontDir = "Wavefront/stove1/";
+    std::string fileName = "stove1.obj";
     objLoader.LoadOBJ(wavefrontDir, fileName);
 
     QSurfaceFormat format;
